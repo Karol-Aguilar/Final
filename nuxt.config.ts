5d9d5b4510ac1d61f357ea5a904a80399ef29e35
Karol-Aguilar <2201080004@undc.edu.pe>
2025-03-15 12:15:16 -0500

    Actualizacion1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  nitro: {
+    preset: 'vercel',
+  },
   css: [
     'bootstrap/dist/css/bootstrap.min.css',
     'bootstrap-icons/font/bootstrap-icons.css' // Agregar Bootstrap Icons
